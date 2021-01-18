1ed63c38dc3c80fb5c6f5af7c18001d869f6a723
David Vilaça <vilacapdavid@gmail.com>
2021-01-17 21:06:31 -0300

    feat(ports): add lambda port

--- debug-lambda.sh
+++ debug-lambda.sh
@@ -1,7 +1,7 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  aws lambda invoke /dev/null --endpoint-url=http://localhost:4566 --function-name todo_tf_handler --payload $(cat mocks/lambda/events/create_todo.json | base64 -w 0)
+  aws lambda invoke /dev/null --endpoint-url=http://localhost:3002 --function-name todo_tf_handler --payload $(cat mocks/lambda/events/create_todo.json | base64 -w 0)
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  aws lambda invoke /dev/null --endpoint-url=http://localhost:4566 --function-name todo_tf_handler --payload $(cat mocks/lambda/events/create_todo.json | base64 -w 0)
+  aws lambda invoke /dev/null --endpoint-url=http://localhost:3002 --function-name todo_tf_handler --payload $(cat mocks/lambda/events/create_todo.json | base64 -w 0)
 else
   echo "༼つಠ益ಠ༽つ ─=≡ΣO)) NOT NOW"
 fi
